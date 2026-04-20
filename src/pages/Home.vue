bcdd0c499b4ba467bfbd79973afcc993020fec29
Maria Khan <hb4343651@gmail.com>
2026-04-20 11:36:55 -0700

    monday

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,6 +10,4 @@ import FooterComponent from '@/components/Footer-component.vue';
 import NewsletterComponent from '@/components/Newsletter-component.vue';
 import PostComponent from '@/components/Post-component.vue';
 import WelcomeComponent from '@/components/Welcome-component.vue';
-
-
 </script>
